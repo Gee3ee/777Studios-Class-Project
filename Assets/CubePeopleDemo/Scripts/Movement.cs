bcd5fa50b32313e130ee57a93dde6299d4f5d5c3
Daniel Esparza <32282181+desparza90@users.noreply.github.com>
2021-10-06 19:38:50 -0700

    Fixed jump and player rotation
    When pressing the right and left arrow keys player rotates correctly now and jump works better. These changes are made to the movement script in cube people scripts

--- Assets/CubePeopleDemo/Scripts/Movement.cs
+++ Assets/CubePeopleDemo/Scripts/Movement.cs
@@ -5,32 +5,53 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     float movementspeed = 7.5f;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        float yRotation = transform.eulerAngles.y;
+
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (transform.localRotation.y == 0)
-                transform.Rotate(0, 180, 0);
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && yRotation == 0)
+            {
+                transform.RotateAround(transform.position, transform.up, 180f);
+            }
             transform.Translate(Vector3.back * movementspeed * Time.deltaTime, Space.World);
         }
+        
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (transform.localRotation.y <= 1)
-                transform.Rotate(0, 0, 0);
+            if (Input.GetKeyDown(KeyCode.RightArrow) && yRotation != 0)
+            {
+                transform.RotateAround(transform.position, transform.up, 180f);
+            }
             transform.Translate(Vector3.forward * movementspeed * Time.deltaTime, Space.World);
-            
         }
-        if (Input.GetKey(KeyCode.Space))
+        
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            //transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
         }
     }
     private void OnTriggerEnter(Collider other)
